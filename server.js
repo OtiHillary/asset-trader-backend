d3602de94842abdeec25406feb792285a462ae53
otonye hillary <otonye.dev@gmail.com>
2025-02-11 21:11:59 +0100

    created login endpoint

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
+app.post('/login', (req, res) =>{
+  try {
+    const { username, password } = req.body
+    //check for user in db here
+
+    if ( username /*username found in db*/ ) {
+      if (/*Hash.password == db.passwordHash*/ password ) {
+        res.status(200).json({ message : "logged in successful" })        
+      } else {
+        res.status(400).json({ message : "incorrect username or password" })        
+      }
+    } else {
+      res.status(404).json({ message : "user does not exist" })        
+    }
+
+  } catch (error) {
+    console.log(error)    
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
